2bcc6acb2e3d386a9b596e1c231bc8dfcfc76f01
Cosmin Dumitru <cosu@cosu.ro>
2016-11-02 23:45:49 +0100

    add support for dynamic updates

--- content_script.js
+++ content_script.js
@@ -1,10 +1,31 @@
-var node;
-var walk = document.createTreeWalker(document.body,NodeFilter.SHOW_TEXT,null,false);
+var regex = /Liviu\s+(Nicolae)?\s*Dragnea/gi 
 
-while(node=walk.nextNode()) {
+function dragnea(node) {
+    var walk = document.createTreeWalker(node,NodeFilter.SHOW_TEXT,null,false);
+    var node;
+
+    while(node=walk.nextNode()) {
     var text = node.nodeValue;
-    if (text.search(/Liviu Dragnea/gi) >= 0)
-    {   
-        node.nodeValue = text.replace(/Liviu\s+Dragnea/gi, 'Infractorul condamnat definitiv Liviu Dragnea');
-    }    
+    if (text.search(regex) >= 0) {   
+        node.nodeValue = text.replace(regex, 'Infractorul condamnat definitiv Liviu Dragnea');
+        }    
+    }
 }
+
+
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.addedNodes.length) {
+      for ( var addedNode of mutation.addedNodes) {
+        dragnea(addedNode);
+      }
+    }
+  })
+});
+
+
+// because facebook
+observer.observe(document.body, { childList: true,  subtree: true });
+
+dragnea(document.body);
