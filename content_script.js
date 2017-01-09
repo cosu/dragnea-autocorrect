954c8e1f7fee63522e71add3236e6bebfd888d24
Cosmin Dumitru <cosu@cosu.ro>
2017-01-09 22:03:34 +0100

    add guard to regex to prevent recursion

--- content_script.js
+++ content_script.js
@@ -1,15 +1,15 @@
-var regex = /Liviu\s+(Nicolae)?\s*Dragnea/gi 
+var regex = /((?!definitiv))(Liviu)?\s?(Nicolae)?\s*Dragnea/gi 
 
-function dragnea(node) {
+function dragnify(node) {
     var walk = document.createTreeWalker(node,NodeFilter.SHOW_TEXT,null,false);
     var node;
 
     while(node=walk.nextNode()) {
-    var text = node.nodeValue;
-    if (text.search(regex) >= 0) {   
-        node.nodeValue = text.replace(regex, 'Infractorul condamnat definitiv Liviu Dragnea');
-        }    
-    }
+      var text = node.nodeValue;
+      if (text.search(regex) >= 0) {   
+            node.nodeValue = text.replace(regex, ' infractorul condamnat definitiv Liviu Dragnea');
+          }    
+      }
 }
 
 
@@ -18,14 +18,14 @@ var observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
     if (mutation.addedNodes.length) {
       for ( var addedNode of mutation.addedNodes) {
-        dragnea(addedNode);
+        dragnify(addedNode);
       }
     }
   })
 });
 
 
-// because facebook
+// because facebook and friends modify the dom after page load
 observer.observe(document.body, { childList: true,  subtree: true });
 
-dragnea(document.body);
+dragnify(document.body);
